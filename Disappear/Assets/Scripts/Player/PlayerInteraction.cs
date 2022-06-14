03ca4c92477baa789b7cb9c3463df7b209742992
Aedrinios <clementclerc2@gmail.com>
2022-06-15 00:43:02 +0200

    change raycast to boxcast (needs adjusting)

--- Disappear/Assets/Scripts/Player/PlayerInteraction.cs
+++ Disappear/Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,25 +1,33 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Matrix4x4 = UnityEngine.Matrix4x4;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerInteraction : MonoBehaviour
 {
     #region Class Members
 
-    [Header("Interaction Parameters")] 
+    [SerializeField] private Transform cam;
 
-    [Tooltip("Layer of objects that can be interacted with")] [SerializeField]
+    [Header("Interaction Parameters")] [Tooltip("Layer of objects that can be interacted with")] [SerializeField]
     private LayerMask layer;
 
     [Tooltip("Maximum distance to detect interactable objects")] [SerializeField]
     private int interactMaxDistance;
 
-    public Interactable interactableObject;
-    private Transform cam;
+    [Tooltip("The size of the cube for detecting interactable objects")] [SerializeField]
+    private Vector3 interactCubeSize;
+
+
+    private Interactable interactableObject;
     private RaycastHit hit;
-    private Ray camRay;
+    private Vector3 camRay;
     private GameObject player;
+
+    bool m_HitDetect;
 
     // Start is called before the first frame update
 
@@ -27,14 +35,14 @@ public class PlayerInteraction : MonoBehaviour
 
     #region Unity Functions
 
-    void Awake()
+    private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         cam = transform.parent;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetButtonDown("Interact") && interactableObject != null)
         {
@@ -42,13 +50,12 @@ public class PlayerInteraction : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        camRay = new Ray(cam.position, cam.forward);
-
-        if (Physics.Raycast(camRay, out hit, interactMaxDistance, layer))
+        m_HitDetect = Physics.BoxCast(transform.position, interactCubeSize, cam.forward, out hit,
+            cam.rotation, interactMaxDistance, layer);
+        if (m_HitDetect)
         {
-            Debug.Log("Found");
             if (!hit.collider.TryGetComponent(out interactableObject))
             {
                 interactableObject = null;
@@ -57,6 +64,28 @@ public class PlayerInteraction : MonoBehaviour
         else
         {
             interactableObject = null;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        //Check if there has been a hit yet
+        if (m_HitDetect)
+        {
+            //Draw a Ray forward from GameObject toward the hit
+            Gizmos.DrawRay(transform.position, cam.forward * hit.distance);
+            //Draw a cube that extends to where the hit exists
+            Gizmos.DrawWireCube(transform.position + cam.forward * hit.distance, interactCubeSize);
+        }
+        //If there hasn't been a hit yet, draw the ray at the maximum distance
+        else
+        {
+            //Draw a Ray forward from GameObject toward the maximum distance
+            Gizmos.DrawRay(transform.position, cam.forward * interactMaxDistance);
+            //Draw a cube at the maximum distance
+            Gizmos.DrawWireCube(transform.position + cam.forward * interactMaxDistance, interactCubeSize);
         }
     }
 
