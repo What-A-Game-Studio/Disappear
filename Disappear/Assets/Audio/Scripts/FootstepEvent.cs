0f0731f83093274aae7baa66bac4af475004e7eb
Aedrinios <clementclerc2@gmail.com>
2022-07-02 18:25:45 +0200

    fixed double sound on walk or run

--- Disappear/Assets/Audio/Scripts/FootstepEvent.cs
+++ Disappear/Assets/Audio/Scripts/FootstepEvent.cs
@@ -3,19 +3,17 @@ using UnityEngine;
 
 public class FootstepEvent : MonoBehaviour
 {
-    [Header("Footstep Sound Parameters")] 
-    
-    [SerializeField]
+    [Header("Footstep Sound Parameters")] [SerializeField]
     private float pitchVariation;
 
     [SerializeField] private AudioClip defaultFootStep;
     private SurfaceTypeSO surfaceType;
-
+    public bool IsPlaying { get; set; } = false;
     private Transform feet;
 
     private void Awake()
     {
-     //   feet = transform.parent.GetChild(transform.GetSiblingIndex() + 1);
+        //   feet = transform.parent.GetChild(transform.GetSiblingIndex() + 1);
     }
 
     public void Init(Transform f)
@@ -25,13 +23,18 @@ public class FootstepEvent : MonoBehaviour
 
     public void PlayFootstepSound()
     {
-        AudioClip toPlay = (surfaceType && surfaceType.FootstepOnSurface.Count > 0)
-            ? surfaceType.FootstepOnSurface[Random.Range(0, surfaceType.FootstepOnSurface.Count)]
-            : defaultFootStep;
-        
-        AudioManager.Instance.PlaySpatializedSoundOnce(
-            toPlay, feet.position,
-            1 + Random.Range(-pitchVariation, pitchVariation));
+        Debug.Log("Trying to make sound");
+        if (!IsPlaying)
+        {
+            IsPlaying = true;
+            AudioClip toPlay = (surfaceType && surfaceType.FootstepOnSurface.Count > 0)
+                ? surfaceType.FootstepOnSurface[Random.Range(0, surfaceType.FootstepOnSurface.Count)]
+                : defaultFootStep;
+
+            AudioManager.Instance.PlaySpatializeSoundOnce(this,
+                toPlay, feet.position,
+                1 + Random.Range(-pitchVariation, pitchVariation));
+        }
     }
 
     public void ChangeSurfaceType(SurfaceTypeSO st)
