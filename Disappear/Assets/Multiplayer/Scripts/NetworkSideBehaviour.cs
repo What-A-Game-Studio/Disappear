62651b520d5a73c8d2c6d25e40a01b6a1de7d53d
Aedrinios <clementclerc2@gmail.com>
2023-01-24 17:12:38 +0100

    :wrench: :file_folder: clean NetworkSideBehaviour script and add LateUpdate Client and Server functions

--- Disappear/Assets/Multiplayer/Scripts/NetworkSideBehaviour.cs
+++ Disappear/Assets/Multiplayer/Scripts/NetworkSideBehaviour.cs
@@ -11,14 +11,9 @@ namespace WAG.Multiplayer
         public override void OnNetworkSpawn()
         {
             if (IsServer)
-            {
                 OnServerSpawn();
-            }
-            
             if (IsClient)
-            {
                 OnClientSpawn();
-            }
         }
 
         public override void OnNetworkDespawn()
@@ -29,95 +24,41 @@ namespace WAG.Multiplayer
                 OnClientDespawn();
             OnDespawn();
         }
-
-        protected virtual void OnClientDespawn()
-        {
-        }
-
-        protected virtual void OnDespawn()
-        {
-        }
-        protected virtual void OnServerDespawn()
-        {
-        }
-
+        
         void Update()
         {
             if (IsServer)
-            {
                 UpdateServer();
-            }
-
             if (IsClient)
-            {
                 UpdateClient();
-            }
         }
 
         private void FixedUpdate()
         {
             if (IsServer)
-            {
                 FixedUpdateServer();
-            }
-
             if (IsClient)
-            {
                 FixedUpdateClient();
-            }
         }
         
         private void LateUpdate()
         {
             if (IsServer)
-            {
                 LateUpdateServer();
-            }
-            
             if (IsClient)
-            {
                 LateUpdateClient();
-            }
-        }
-
-        protected virtual void OnClientSpawn()
-        {
-        }
-
-        protected virtual void OnServerSpawn()
-        {
-        }
-
-        protected virtual void UpdateServer()
-        {
-        }
-
-        protected virtual void UpdateClient()
-        {
-        }
-
-        protected virtual void FixedUpdateServer()
-        {
-        }
-
-        protected virtual void FixedUpdateClient()
-        {
         }
         
-        protected virtual void LateUpdateServer()
-        {
-        }
-
-        protected virtual void LateUpdateClient()
-        {
-        }
-
-
         protected virtual void OnClientSpawn(){}
         protected virtual void OnServerSpawn(){}
+        protected virtual void OnClientDespawn(){}
+        protected virtual void OnServerDespawn(){}
+        protected virtual void OnDespawn(){}
         protected virtual void UpdateServer(){}
         protected virtual void UpdateClient(){}
         protected virtual void FixedUpdateServer(){}
         protected virtual void FixedUpdateClient(){}
+        protected virtual void LateUpdateServer(){}
+        protected virtual void LateUpdateClient(){}
     }
 }
